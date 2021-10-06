f0da2b231e3521a3ddac63929966326c3629dbb6
Mauricio <robayo.m.antonio@gmail.com>
2021-10-06 15:43:23 -0400

    remove comments, initialize datamodule

--- mlp/train.py
+++ mlp/train.py
@@ -2,7 +2,7 @@ import hydra
 from hydra.core.config_store import ConfigStore
 import pytorch_lightning as pl
 import pytorch_lightning.callbacks
-from . import model
+from . import dataset, model
 
 @hydra.main(config_name='conf', config_path=None)
 def main(config):
@@ -15,18 +15,20 @@ def main(config):
     trainer_kwargs['gpus'] = config.gpus
     trainer_kwargs['callbacks'] = callbacks
     trainer_kwargs['max_epochs'] = config.max_epochs
+
     if config.gpus > 1:
         trainer_kwargs['accelerator'] = 'ddp'
+
     trainer = pytorch_lightning.Trainer(**trainer_kwargs)
+    dm = dataset.DataModule(config.batch_size // config.gpus,
+                            config.data.datafile)
+    dm.setup()
+    config.data.dataset_size = len(dm.train_dataset)
+    config.model.character_size = dm.character_vocab_size
+    config.model.phoneme_size = dm.phoneme_vocab_size
 
-    """
-    need to add portion of code to setup data and loaders, etc
-    """
-
-    config.data.dataset_size = len()
-    model = model.MLP(config)
-    # remember to add necessary data module here.
-    trainer.fit(model, datamodule=)
+    word_2_phone_model = model.MLP(config)
+    trainer.fit(word_2_phone_model, datamodule=dm)
 
 
 if __name__ == '__main__':
