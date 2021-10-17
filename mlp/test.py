985482b31e5610920189b6bfa5fb9f0f40d32c6a
Sammie Kim--email=sk7327@nyu.edu <sk7327@b-17-38.c.hpc-slurm-9c75.internal>
2021-10-17 12:18:01 -0400

    adding test file

--- mlp/test.py
+++ mlp/test.py
@@ -22,8 +22,9 @@ def main(config):
 
 
     word_2_phone_model = model.MLP(config)
-    # model_test = word_2_phone_model.load_from_checkpoint(checkpoint_path="/content/gdrive/MyDrive/2021_Capstone/capstone-project-47/outputs/capstone-epoch=49-val/loss=1.03.ckpt")
+    #model_test = word_2_phone_model.load_from_checkpoint(checkpoint_path="/home/sk7327/cds-bootcamp/lecture2/capstone-project-47/capstone-project-47/outputs/2021-10-16/16-30-46/lightning_logs/version_0/checkpoints/capstone-spanish-epoch=02-val/loss=0.17.ckpt")
     model_test = word_2_phone_model.load_from_checkpoint(checkpoint_path=config.dir.load_path)
+
 
     trainer = pl.Trainer()  
     trainer.test(model_test, datamodule=dm, verbose=True)
